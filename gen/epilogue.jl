a7a7856982bc557a3a0b0bbbcb1b448d0e991583
David MacMahon <davidm@astro.berkeley.edu>
2025-07-02 19:34:29 -0700

    Improve API generator and regen src/api.jl
    Some functions in `verbs.h` are static inline functions.  These are not
    available as standaline functions in the shared library so we must
    copy their implementations.

--- gen/epilogue.jl
+++ gen/epilogue.jl
@@ -5,3 +5,35 @@ export ibv_query_port
 function ibv_query_port(context, port_num, port_attr)
     ccall((:ibv_query_port, libibverbs), Cint, (Ptr{ibv_context}, UInt8, Ptr{ibv_port_attr}), context, port_num, port_attr)
 end
+
+# Functions that are static inline in verbs.h
+
+# Get ibv_context_ops struct from an Ptr{ibv_context}
+function get_ibv_context_op(ctx::Ptr{ibv_context}, op::Symbol)
+    op_offset = fieldoffset(ibv_context_ops, findfirst(==(op), fieldnames(ibv_context_ops)))
+    unsafe_load(Ptr{Ptr{Cvoid}}(ctx + fieldoffset(ibv_context, 2) + op_offset))
+end
+
+function get_ibv_context_op(cq::Ptr{ibv_cq}, op::Symbol)
+    ctx = unsafe_load(Ptr{Ptr{ibv_context}}(cq))
+    get_ibv_context_op(ctx, op)
+end
+
+export ibv_poll_cq
+
+function ibv_poll_cq(cq, num_entries, wc)
+    ccall(get_ibv_context_op(cq, :poll_cq), Cint, (Ptr{ibv_cq}, Cint, Ptr{ibv_wc}), cq, num_entries, wc)
+end
+
+export ibv_req_notify_cq
+
+function ibv_req_notify_cq(cq, solicited_only)
+    ccall(get_ibv_context_op(cq, :req_notify_cq), Cint, (Ptr{ibv_cq}, Cint), cq, solicited_only)
+end
+
+#= TODO
+export ibv_modify_cq
+function ibv_modify_cq(cq, attr)
+    ccall((:ibv_modify_cq, libibverbs), Cint, (Ptr{ibv_cq}, Ptr{ibv_modify_cq_attr}), cq, attr)
+end
+=#
--- src/api.jl
+++ src/api.jl
@@ -2699,18 +2699,6 @@ function ibv_ack_cq_events(cq, nevents)
     ccall((:ibv_ack_cq_events, libibverbs), Cvoid, (Ptr{ibv_cq}, Cuint), cq, nevents)
 end
 
-function ibv_poll_cq(cq, num_entries, wc)
-    ccall((:ibv_poll_cq, libibverbs), Cint, (Ptr{ibv_cq}, Cint, Ptr{ibv_wc}), cq, num_entries, wc)
-end
-
-function ibv_req_notify_cq(cq, solicited_only)
-    ccall((:ibv_req_notify_cq, libibverbs), Cint, (Ptr{ibv_cq}, Cint), cq, solicited_only)
-end
-
-function ibv_modify_cq(cq, attr)
-    ccall((:ibv_modify_cq, libibverbs), Cint, (Ptr{ibv_cq}, Ptr{ibv_modify_cq_attr}), cq, attr)
-end
-
 function ibv_create_srq(pd, srq_init_attr)
     ccall((:ibv_create_srq, libibverbs), Ptr{ibv_srq}, (Ptr{ibv_pd}, Ptr{ibv_srq_init_attr}), pd, srq_init_attr)
 end
@@ -3034,6 +3022,38 @@ export ibv_query_port
 function ibv_query_port(context, port_num, port_attr)
     ccall((:ibv_query_port, libibverbs), Cint, (Ptr{ibv_context}, UInt8, Ptr{ibv_port_attr}), context, port_num, port_attr)
 end
+
+# Functions that are static inline in verbs.h
+
+# Get ibv_context_ops struct from an Ptr{ibv_context}
+function get_ibv_context_op(ctx::Ptr{ibv_context}, op::Symbol)
+    op_offset = fieldoffset(ibv_context_ops, findfirst(==(op), fieldnames(ibv_context_ops)))
+    unsafe_load(Ptr{Ptr{Cvoid}}(ctx + fieldoffset(ibv_context, 2) + op_offset))
+end
+
+function get_ibv_context_op(cq::Ptr{ibv_cq}, op::Symbol)
+    ctx = unsafe_load(Ptr{Ptr{ibv_context}}(cq))
+    get_ibv_context_op(ctx, op)
+end
+
+export ibv_poll_cq
+
+function ibv_poll_cq(cq, num_entries, wc)
+    ccall(get_ibv_context_op(cq, :poll_cq), Cint, (Ptr{ibv_cq}, Cint, Ptr{ibv_wc}), cq, num_entries, wc)
+end
+
+export ibv_req_notify_cq
+
+function ibv_req_notify_cq(cq, solicited_only)
+    ccall(get_ibv_context_op(cq, :req_notify_cq), Cint, (Ptr{ibv_cq}, Cint), cq, solicited_only)
+end
+
+#= TODO
+export ibv_modify_cq
+function ibv_modify_cq(cq, attr)
+    ccall((:ibv_modify_cq, libibverbs), Cint, (Ptr{ibv_cq}, Ptr{ibv_modify_cq_attr}), cq, attr)
+end
+=#
 
 
 # exports
